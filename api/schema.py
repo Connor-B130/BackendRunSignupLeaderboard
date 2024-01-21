2653f212f0b08504e21b67473f6d35a0b350e96a
Connor Bennett <s1305818@monmouth.edu>
2024-01-21 17:33:39 -0500

    Initial Commit

--- api/schema.py
+++ api/schema.py
@@ -3,14 +3,18 @@ import urllib.request, json
 
 baseURL = "https://runsignup.com/Rest/"
 
-query = QueryType()
-
-@query.field("Races")
 def resolve_races(*_):
-    response = urllib.request.urlopen(baseURL + 'races?format=json')
-    data = response.read()
-    dict = json.loads(data)
-    #print(dict["races"])
-    #print(type(dict))
-    print(data)
-    return dict["races"]
+    try:
+        response = urllib.request.urlopen(baseURL + 'races?format=json')
+        data = response.read()
+        dict = [json.loads(data)]
+        payload = {
+            "success": True,
+            "races": dict
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
+        }
+    return payload
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from api import app
 from ariadne import load_schema_from_path, make_executable_schema, \
     graphql_sync, snake_case_fallback_resolvers, ObjectType, gql
-from ariadne.constants import PLAYGROUND_HTML
+from ariadne.explorer import ExplorerGraphiQL
 from flask import request, jsonify
 from api.schema import resolve_races
 import redis
@@ -22,7 +22,7 @@ schema = make_executable_schema(
 
 @app.route("/graphql", methods=["GET"])
 def graphql_playground():
-    return PLAYGROUND_HTML, 200
+    return ExplorerGraphiQL, 200
 
 
 @app.route("/graphql", methods=["POST"])
